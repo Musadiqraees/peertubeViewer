5ad3d3472a9a7ad2a5d0e2c8a2f4e2b200fae3ac
musadiqraees <musadiqraees@gmail.com>
2020-04-06 20:50:59 +0200

    added no ssl

--- src/backend/network/curl_request.cpp
+++ src/backend/network/curl_request.cpp
@@ -39,7 +39,7 @@ string curl_request(string url)
         curl_easy_setopt(curl,CURLOPT_URL,url.c_str());
         curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,write_data);
         curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
         curl_easy_setopt(curl,CURLOPT_WRITEDATA,&return_stream);
         CURLcode returnValue = curl_easy_perform(curl);
 
